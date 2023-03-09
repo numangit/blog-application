e59a8349af620f1188bac1a83911910e15991713
numangit <abdulnumanjob@gmail.com>
2023-03-09 15:48:37 +0600

    home component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/home/blog/common/Navigation';
+import Blog from './pages/Blog';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div >
       <Navigation />
+      <Home />
+      <Blog />
     </div>
   );
 }
